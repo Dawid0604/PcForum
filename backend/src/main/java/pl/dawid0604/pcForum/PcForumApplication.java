1ec2977202150f96b3fd3739228d763dfbe85547
Dawid Sikora <dawid.sikora@investoro.pl>
2024-11-11 20:49:41 +0100

    Initial commit

---
+++ backend/src/main/java/pl/dawid0604/pcForum/PcForumApplication.java
@@ -0,0 +1,13 @@
+package pl.dawid0604.pcForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcForumApplication.class, args);
+	}
+
+}
